942cef8832acf1f4e175d706c7443a7af89df3dc
ikaro <ikaroassis08@gmail.com>
2022-09-28 21:58:57 -0300

    Aea

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// function pdf(){
+//     const form=document.getElementById('form').innerHTML
+//     var janela = window.open('','','width-800,height=600')
+//     janela.document.write('<html></html>')
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        calibri_font:'calibri, sans',
+        arial_font:'arial, sans'
+      }
+    },
+  },
+  plugins: [],
+}
